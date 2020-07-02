7c5717260813c6e9f0812af63cbb17202359bf3b
jha94archit <jha94archit@gmail.com>
2020-07-02 17:39:45 +0530

    Initial commit

---
+++ full_throttle_labs/full_throttle_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FullThrottleAppConfig(AppConfig):
+    name = 'full_throttle_app'
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/architjha/Documents/FullThrottle/DjangoProj/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
